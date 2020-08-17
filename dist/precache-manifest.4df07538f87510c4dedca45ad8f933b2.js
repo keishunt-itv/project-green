b38d57898bfa85cda61701c6c614694a220f4c66
keishunt-itv <keisha.hunt@itv.com>
2020-08-17 15:43:35 +0100

    changes setup so popup working better

---
+++ dist/precache-manifest.4df07538f87510c4dedca45ad8f933b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4520ac23cdcb9bf60274a449beae013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c515b10794cc83f8c52e",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "a04292a92b739b4af1de",
+    "url": "/static/js/2.18a62067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18a62067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c515b10794cc83f8c52e",
+    "url": "/static/js/main.24dbda36.chunk.js"
+  },
+  {
+    "revision": "f1a76b6035e50eefc4ff",
+    "url": "/static/js/runtime-main.c565f299.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
