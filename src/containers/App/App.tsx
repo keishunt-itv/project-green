c0c8e46656371ebd5b0c4781d48aad8d850a3f09
keishunt-itv <keisha.hunt@itv.com>
2020-08-18 09:19:58 +0100

    edits popup size

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,14 +1,14 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import logo from '../../resources/images/logo.svg';
 import './App.css';
 
-const App : FC = () => {
+const App: FC = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-          Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
@@ -16,7 +16,7 @@ const App : FC = () => {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-          Learn React
+                    Learn React
                 </a>
             </header>
         </div>
